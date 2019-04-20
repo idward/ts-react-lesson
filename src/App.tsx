6e38fa022369d23f0bc7343ed3d893377d96ef85
idward <idward@vip.sina.com>
2019-04-20 14:22:23 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import React, { PureComponent } from "react";
+import React from "react";
 
-class App extends PureComponent {
-  sum = (a: number, b: number): number => {
-    return a + b;
-  };
-
-  public render() {
-    return (
-      <div>
-        <h1>Hello {this.sum(3, 8)}</h1>
-      </div>
-    );
-  }
-}
+const App = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
 
 export default App;
